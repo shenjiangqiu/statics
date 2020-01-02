2c121f4811d687720c2ad21cbf68fd82420d2444
Jiangqiu Shen <jshen2@mtu.edu>
2020-01-02 11:48:52 -0500

    init

---
+++ b1904P1-8x8c6h5SAT.cnf.unfix/sim.scripts.py
@@ -0,0 +1,3 @@
+import sys
+sys.argv = [ "/tmp/sjq/sniper/scripts/fixaddress.py", "" ]
+execfile("/tmp/sjq/sniper/scripts/fixaddress.py")
---
+++ sha1r17m72a.cnf.fix/sim.scripts.py
@@ -0,0 +1,3 @@
+import sys
+sys.argv = [ "/home/jshen/git/sniper/scripts/fixaddress.py", "" ]
+execfile("/home/jshen/git/sniper/scripts/fixaddress.py")
